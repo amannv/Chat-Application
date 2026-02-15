d2c1154931a693ab19bf9359e6c0aeb20881f156
Aman <aman.ver9345@gmail.com>
2026-02-15 14:23:20 +0530

    wrote maps for storing sockets and user and roomid

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,37 @@
 import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
-const User = new Map<string, string>(); 
-const roomId = new Map<string, number>();
+const socketToUser = new Map<WebSocket, string>();
+const userToroomId = new Map<string, number>();
+const roomToUsers = new Map<number, Set<string>>();
 
 wss.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    const parsedMessage = JSON.parse(message as unknown as string);
 
-    socket.on("message", (message) => {
-        const parsedMessage = JSON.parse(message as unknown as string);
-        if (parsedMessage.type == "join") {
-            console.log("User joined " + parsedMessage.payload.username);
-            User.set("username", parsedMessage.payload.username);
-            roomId.set("roomId", parsedMessage.payload.roomId);
-            console.log(User.get("username"));
-            console.log(roomId.get("roomId"));
-        }
-    })
-})
+    if (parsedMessage.type == "join") {
+      console.log(
+        "User joined this room " +
+          parsedMessage.payload.roomId +
+          " " +
+          "username is " +
+          parsedMessage.payload.username,
+      );
+    }
+
+    const username = parsedMessage.payload.username;
+    const roomId = parsedMessage.payload.roomId;
+
+    socketToUser.set(socket, username);
+    userToroomId.set(username, roomId);
+
+    if (!roomToUsers.has(roomId)) {
+      roomToUsers.set(roomId, new Set());
+    }
+
+    const users = roomToUsers.get(roomId);
+    if (users) {
+      users.add(username);
+    }
+  });
+});
