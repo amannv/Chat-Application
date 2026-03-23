1c045df58664ba491ad656456e5263aeefa05d5b
Aman <aman.ver9345@gmail.com>
2026-03-23 19:44:57 +0530

    completed the whole flow of message

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,59 +1,48 @@
 import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
-const socketToUser = new Map<WebSocket, string>();
-const userToroomId = new Map<string, number>();
-const roomToUsers = new Map<number, Set<WebSocket>>();
+const socketToRoomId = new Map<WebSocket, Number>();
+const roomToUsers = new Map<Number, Set<WebSocket>>();
 
 wss.on("connection", (socket) => {
   socket.on("message", (message) => {
     const parsedMessage = JSON.parse(message as unknown as string);
 
     if (parsedMessage.type == "join") {
-      console.log(
-        parsedMessage.payload.username +
-          " created and joined room with id " +
-          parsedMessage.payload.roomId,
-      );
+      console.log("User joined room with id " + parsedMessage.payload.roomId);
 
-      const username = parsedMessage.payload.username;
       const roomId = parsedMessage.payload.roomId;
 
-      socketToUser.set(socket, username);
-      userToroomId.set(username, roomId);
+      socketToRoomId.set(socket, roomId);
 
       if (!roomToUsers.has(roomId)) {
         roomToUsers.set(roomId, new Set());
       }
 
-      const users = roomToUsers.get(roomId);
-      if (users) {
-        users.add(socket);
+      const sockets = roomToUsers.get(roomId);
+      if (sockets) {
+        sockets.add(socket);
       }
     }
 
     if (parsedMessage.type == "chat") {
       const chatMessage = parsedMessage.payload.message;
-      const username = socketToUser.get(socket);
 
-      if (!username) {
-        return console.error("User don't exist");
-      }
-
-      const roomId = userToroomId.get(username);
+      const roomId = socketToRoomId.get(socket);
 
       if (!roomId) {
         return console.error("Room not exists");
       }
 
-      const users = roomToUsers.get(roomId);
+      const sockets = roomToUsers.get(roomId);
 
-      if(!users) {
+      if (!sockets) {
         return console.error("No users exists in the room");
       }
 
-      console.log(chatMessage);
-      socket.send(chatMessage);
+      sockets.forEach((socket) => {
+        socket.send(chatMessage);
+      });
     }
   });
 });
