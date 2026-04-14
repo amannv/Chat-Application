2ea8ff6ed9dbbab5655f18e42f895f4096a24f8e
Aman <aman.ver9345@gmail.com>
2026-04-14 23:25:46 +0530

    wrote some more code

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -40,8 +40,9 @@ const LandingPage = () => {
   return (
     <div className="h-screen max-w-[23vw] mx-auto flex justify-center items-center">
       <div className="w-full flex flex-col justify-center items-center p-4 rounded-sm bg-neutral-950 border border-neutral-900 gap-2">
-        <div className="text-l text-foreground mb-2 font-jetbrains font-semibold">
-          Create your chat room
+        <div className="text-white font-jetbrains text-2xl font-bold">Chat-Flow</div>
+        <div className="text-sm text-foreground mb-2 font-jetbrains">
+          Real-time chat made simple
         </div>
         <InputBox
           ref={usernameInput}
