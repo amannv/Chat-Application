2dd46fffb9e757a50b49b34b07aaa7ad81c4354e
Aman <aman.ver9345@gmail.com>
2026-05-01 08:10:17 +0530

    created context

--- frontend/src/context/Context.tsx
+++ frontend/src/context/Context.tsx
@@ -1,7 +1,45 @@
-import React, { createContext } from "react";
+import React, { createContext, useRef, useState } from "react";
 
-export const userContext = createContext();
+type userContextType = {
+  username: string | undefined;
+  roomId: string | undefined;
+  socket: WebSocket | null;
+  setUser: (username: string | undefined, roomId: string | undefined) => void;
+  connectSocket: () => void;
+};
 
-export const userData = ({ children }: React.ReactNode) => {
+export const userContext = createContext<userContextType | undefined>(
+  undefined,
+);
 
-}
+export const UserData = ({ children }: { children: React.ReactNode }) => {
+  const [username, setUsername] = useState<string | undefined>();
+  const [roomId, setRoomId] = useState<string | undefined>();
+  const socket = useRef<WebSocket | null>(null);
+
+  const setUser = (
+    username: string,
+    roomId: string,
+  ) => {
+    if(!username && !roomId) return null;
+    setUsername(username);
+    setRoomId(roomId);
+  };
+
+  const connectSocket = () => {
+    if (socket.current == undefined) {
+    const ws = new WebSocket("ws://localhost:3000");
+    socket.current = ws;
+    } else {
+        return null;
+    }
+  };
+
+  return (
+    <userContext.Provider
+      value={{ username, roomId, setUser, socket.current, connectSocket }}
+    >
+      {children}
+    </userContext.Provider>
+  );
+};
